199c26a389df02c302648e0011eb1b1742135f46
Song Li <da_tou_li@yahoo.com>
2020-10-07 17:54:14 -0400

    Initial commit

---
+++ a-count.py
@@ -0,0 +1,30 @@
+
+from pyspark import SparkContext, SparkConf
+from pyspark.sql import Row
+
+if __name__ == "__main__":
+    conf = SparkConf().setAppName("word count") \
+      .setMaster("local[2]")
+
+    context = SparkContext(conf = conf)
+    context.setLogLevel('ERROR')
+
+    # This is to find the number of workers in the cluster. But it is not correct
+    no_of_workers = context.statusTracker().getActiveJobsIds()
+    print('No. of workers = {}'.format(no_of_workers))
+    
+    data = ('This', 'is', 'is', 'cool')
+    rdd = context.parallelize(data, 2).cache()
+    
+    print('No of partitions - {}'.format(rdd.getNumPartitions()))
+    wordCounts = rdd.countByValue()
+    for word, count in wordCounts.items():
+        print("{} : {}".format(word, count))
+
+    print('\nReduce by key:')
+    new_rdd = rdd.map(lambda x: (x,1), preservesPartitioning=True)
+    result = new_rdd.reduceByKey(lambda a, b: a + b).collect()
+
+    print(result)
+    for word, count in result:
+        print("{} : {}".format(word, count))
---
+++ b-load-csv.py
@@ -0,0 +1,30 @@
+
+import sys
+from pyspark import SparkContext, SQLContext, SparkConf
+from pyspark.sql import Row
+
+def run():
+  conf = SparkConf().setAppName('load CSV').setMaster('local[2]')
+
+  context = SparkContext(conf = conf)
+  context.setLogLevel('ERROR')
+  sqlContext = SQLContext(context)
+
+  df_name = sqlContext.read.csv('/home/song/Sandbox/pyspark-example/files/name.csv', header=True)
+  df_score = sqlContext.read.csv('/home/song/Sandbox/pyspark-example/files/score.csv', header=True)
+  df = df_name.join(df_score, on=['Id'], how="left")
+
+  df_name.show()
+  df_score.show()
+  df.show()
+
+  path = '/home/song/Sandbox/pyspark-example/files/name-score.csv'
+  
+  # df.write.csv(path, header=True)
+  df.toPandas().to_csv(path, index=False)
+
+  print(sys.executable)
+  
+# Run the app
+if __name__ == "__main__":
+  run()
